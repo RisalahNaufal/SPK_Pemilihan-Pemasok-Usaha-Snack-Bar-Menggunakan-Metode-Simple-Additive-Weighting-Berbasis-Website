fac489c3aa9ce886f4811ac3606b12469571d874
M. Risalah Naufal <mrnaufal70@gmail.com>
2023-11-30 00:53:50 +0700

    WEBSITE SPK SAW
    Kode untuk website SPK SAW

---
+++ SPK SAW/layout/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+        </div>
+        <div class="float-end">
+            <p>Crafted by <a
+                    href="https://www.instagram.com/reyhanrainaldy">Reyhan Rainaldy</a></p><p>& <a href="https://www.instagram.com/risalahnaufal/">Muhammad Risalah Naufal</a></p>
+            
+                   
+                    
+        </div>
+    </div>
+</footer>
